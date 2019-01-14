dc196481d96b3540fae0705d773d8bfacbe05058
Bertalan Balázs <bertalan10@gmail.com>
2019-01-14 13:33:35 +0100

    #2 Create app skeleton
    skeleton code

--- server.py
+++ server.py
@@ -5,13 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('game.html')
 
-
-@app.route('/game', methods=['POST', 'GET'])
-def game_site():
-    image_num = int(request.args.get('image-num', 3))
-    return render_template('game.html', image_num=image_num)
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,34 @@
+
+
 var app = new Vue({
     el: '#game',
     data: {
-        macilaci: 'Maci nem laci!'
+        macilaci: 'Maci nem laci!',
+        stock1: ['heroe1', 'heroe2', 'heroe3'],
+        playedcard1: {
+            firstrow: '',
+            secondrow: '',
+            thirdrow: ''
+        },
+        selectedCard: ''
     },
     methods: {
         reverseMacilaci: function () {
             this.macilaci = this.macilaci.split('').reverse().join('')
+        },
+        selectCard: function (index) {
+            this.selectedCard = this.stock1[index];
+            this.stock1.splice(index,1)
+        },
+        moveCard: function (key) {
+            this.playedcard1[key] = this.selectedCard;
+            console.log(this.selectedCard);
+            this.selectedCard = ''
         }
     }
 });
+
+
+
 
 
