0b583728116b722b80a0397c30c476e5e11ef3bb
Bertalan Balázs <bertalan10@gmail.com>
2019-01-15 10:39:33 +0100

    player cards are differents , you can play the cards from your hand to the playfield

--- static/js/script.js
+++ static/js/script.js
@@ -1,70 +1,119 @@
 
 
-var app = new Vue({
+let app = new Vue({
     el: '#game',
     data: {
-        macilaci: 'Maci nem laci!',
-        stock1: [{
-            id: 0,
-            name: 'hero1',
-            src: 'static/media/balazs_effect.mp4',
-            hp: 3,
-            attack: 2,
-            mana: 1
+        student: {
+            stock:
+                [{
+                    id: 0,
+                    name: 'hero1',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 3,
+                    attack: 2,
+                    mana: 1
+                },
+                {
+                    id: 1,
+                    name: 'hero2',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 2,
+                    attack: 4,
+                    mana: 2
+                },
+                {
+                    id: 2,
+                    name: 'hero3',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 4,
+                    attack: 1,
+                    mana: 2
+                },
+                {
+                    id: 3,
+                    name: 'hero4',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 2,
+                    attack: 2,
+                    mana: 1
+                },
+                {
+                    id: 4,
+                    name: 'hero5',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 3,
+                    attack: 5,
+                    mana: 3
+                }],
+            playedCard: {
+                firstrow: '',
+                secondrow: '',
+                thirdrow: ''
+            },
+            selectedCard: ''
         },
-        {
-            id: 1,
-            name: 'hero2',
-            src: 'static/media/balazs_effect.mp4',
-            hp: 2,
-            attack: 4,
-            mana: 2
+        mentor: {
+            stock:
+                [{
+                    id: 100,
+                    name: 'hero1',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 3,
+                    attack: 2,
+                    mana: 1
+                },
+                {
+                    id: 101,
+                    name: 'hero2',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 2,
+                    attack: 4,
+                    mana: 2
+                },
+                {
+                    id: 102,
+                    name: 'hero3',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 4,
+                    attack: 1,
+                    mana: 2
+                },
+                {
+                    id: 103,
+                    name: 'hero4',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 2,
+                    attack: 2,
+                    mana: 1
+                },
+                {
+                    id: 104,
+                    name: 'hero5',
+                    src: 'static/media/balazs_effect.mp4',
+                    hp: 3,
+                    attack: 5,
+                    mana: 3
+                }],
+            playedCard: {
+                firstrow: '',
+                secondrow: '',
+                thirdrow: ''
+            },
+            selectedCard: ''
         },
-        {
-            id: 2,
-            name: 'hero3',
-            src: 'static/media/balazs_effect.mp4',
-            hp: 4,
-            attack: 1,
-            mana: 2
-        },
-        {
-            id: 3,
-            name: 'hero4',
-            src: 'static/media/balazs_effect.mp4',
-            hp: 2,
-            attack: 2,
-            mana: 1
-        },
-        {
-            id: 4,
-            name: 'hero5',
-            src: 'static/media/balazs_effect.mp4',
-            hp: 3,
-            attack: 5,
-            mana: 3
-        }
-        ],
-
-        playedcard1: {
-            firstrow: '',
-            secondrow: '',
-            thirdrow: ''
-        },
-        selectedCard: ''
     },
+
     methods: {
-        reverseMacilaci: function () {
-            this.macilaci = this.macilaci.split('').reverse().join('')
+        selectCard: function (id, player) {
+            console.log(player, id);
+            this[player].selectedCard = this[player].stock.find(heroe => heroe.id === id);
+            let index = this[player].stock.findIndex(element => element.id === id); //gives back the index of the selected card
+            this[player].stock.splice(index,1)
         },
-        selectCard: function (id) {
-            this.selectedCard = this.stock1.find(heroe => heroe.id === id);
-            this.stock1.splice(id,1)
-        },
-        moveCard: function (key) {
-            this.playedcard1[key] = this.selectedCard;
-            console.log(this.selectedCard);
-            this.selectedCard = ''
+        moveCard: function (id, player) {
+            this[player].playedCard[id] = this[player].selectedCard;
+            console.log(this[player].selectedCard);
+            this[player].selectedCard = ''
         }
     }
 });
